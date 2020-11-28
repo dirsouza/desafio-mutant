75922c5608bdae43f83bccc1bd6c5bc6fd726da8
Diogo Souza <diogo.souza@uds.com.br>
2020-11-28 18:51:42 -0400

    feat: adicionado config da api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from 'Infrastructure/modules';
+import { apiServer, apiConfig } from 'Infrastructure/servers';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  apiServer(app);
+
+  const { host, port, prefix: apiPrefix } = apiConfig;
+
+  await app.listen(port);
+  logger.debug(`Appliation listening on: http://${host}:${port}/${apiPrefix}`);
 }
 bootstrap();
