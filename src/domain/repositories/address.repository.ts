6f538a25305a880d813834b0f23d0f255e4821a0
Diogo Souza <diogo.souza@uds.com.br>
2020-11-28 22:49:28 -0400

    chore: implementado método de criação no repositorio de endereço

--- src/domain/repositories/address.repository.ts
+++ src/domain/repositories/address.repository.ts
@@ -1,5 +1,19 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { Address } from 'Domain/entities';
+import { AddressDto } from 'Domain/dtos';
 
 @EntityRepository(Address)
-export class AddressRepository extends Repository<Address> {}
+export class AddressRepository extends Repository<Address> {
+  async createAddress(addressDto: AddressDto): Promise<Address> {
+    try {
+      const address = this.create({
+        ...addressDto,
+      });
+
+      return await this.save(address);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+}
