93bb5737c073af4185a2979edac742ac6cd3f5e7
Diogo Souza <diogo.souza@uds.com.br>
2020-11-29 15:44:10 -0400

    refactor: adicionado DTO do Jsonplaceholder

--- src/app/v1/controllers/user.controller.ts
+++ src/app/v1/controllers/user.controller.ts
@@ -2,20 +2,20 @@ import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 import { UserService } from 'Domain/services';
 import { User } from 'Domain/entities';
-import { UserDto } from 'Domain/dtos';
+import { JsonplaceholderDto } from 'Domain/dtos';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  findUsers(): Promise<AxiosResponse<Array<UserDto>>> {
+  findUsers(): Promise<AxiosResponse<Array<JsonplaceholderDto>>> {
     return this.userService.getUsers();
   }
 
   @Post()
   create(
-    @Body(ValidationPipe) createUserDto: Array<UserDto>,
+    @Body(ValidationPipe) createUserDto: Array<JsonplaceholderDto>,
   ): Promise<Array<User>> {
     return this.userService.createUser(createUserDto);
   }
