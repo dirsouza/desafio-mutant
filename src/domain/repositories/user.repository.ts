78cd0cf5f03feb17f02e1e4a783dc066e5280702
Diogo Souza <diogo.souza@uds.com.br>
2020-11-28 23:57:55 -0400

    chore: implementado método de criação no repositorio de usuário

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,5 +1,17 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { UserDto } from 'Domain/dtos';
 import { User } from 'Domain/entities';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(userDto: UserDto): Promise<User> {
+    try {
+      const user = this.create(userDto);
+
+      return await this.save(user);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+}
