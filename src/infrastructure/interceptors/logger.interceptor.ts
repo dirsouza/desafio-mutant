a065765edf404a31706282c3f2fddc7f66d2ce4e
Diogo Souza <diogo.souza@uds.com.br>
2020-11-29 17:36:49 -0400

    feat: adicionado interceptor para gerar logger

---
+++ src/infrastructure/interceptors/logger.interceptor.ts
@@ -0,0 +1,38 @@
+import {
+  Injectable,
+  Inject,
+  NestInterceptor,
+  CallHandler,
+  ExecutionContext,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Logger } from 'winston';
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  constructor(
+    @Inject('winston')
+    private logger: Logger,
+  ) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    this.log(context.switchToHttp().getRequest());
+    return next.handle();
+  }
+
+  private log(req) {
+    const body = { ...req.body };
+
+    this.logger.info({
+      timestamp: new Date().toISOString(),
+      method: req.method,
+      route: req.route.path,
+      data: {
+        body: body,
+        query: req.query,
+        params: req.params,
+      },
+      from: req.ip,
+    });
+  }
+}
