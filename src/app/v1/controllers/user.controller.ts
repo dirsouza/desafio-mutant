3e652306ec9ef3c8c50cc346f0ade2b2201e6e31
Diogo Souza <diogo.souza@uds.com.br>
2020-11-29 12:43:15 -0400

    chore: implementado métodos do controlador para retornar usuários e cadastrar

--- src/app/v1/controllers/user.controller.ts
+++ src/app/v1/controllers/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { UserService } from 'Domain/services';
+import { User } from 'Domain/entities';
+import { UserDto } from 'Domain/dtos';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  findUsers(): Promise<AxiosResponse<Array<UserDto>>> {
+    return this.userService.getUsers();
+  }
+
+  @Post()
+  create(
+    @Body(ValidationPipe) createUserDto: Array<UserDto>,
+  ): Promise<Array<User>> {
+    return this.userService.createUser(createUserDto);
+  }
+}
