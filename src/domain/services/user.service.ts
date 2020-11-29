4e44331dcf57e988736ca5690c4af22fd2cff09a
Diogo Souza <diogo.souza@uds.com.br>
2020-11-29 01:35:59 -0400

    feat: adicionado service de usuário

---
+++ src/domain/services/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Address, Contact, User } from 'Domain/entities';
+import {
+  AddressRepository,
+  ContactRepository,
+  UserRepository,
+} from 'Domain/repositories';
+import { AddressDto, ContactDto, UserDto } from 'Domain/dtos';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    @InjectRepository(AddressRepository)
+    private addressRepository: AddressRepository,
+    @InjectRepository(ContactRepository)
+    private contactRepository: ContactRepository,
+  ) {}
+
+  async createUser(createUser: UserDto): Promise<User> {
+    const address = await this.createAddress(createUser.address);
+    const contact = await this.createContact(createUser.contact);
+
+    return await this.userRepository.createUser(createUser, address, contact);
+  }
+
+  async createAddress(createAddress: AddressDto): Promise<Address> {
+    return await this.addressRepository.createAddress(createAddress);
+  }
+
+  async createContact(createContact: ContactDto): Promise<Contact> {
+    return await this.contactRepository.createContact(createContact);
+  }
+}
