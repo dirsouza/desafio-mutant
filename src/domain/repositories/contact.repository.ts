f5ff03461395c2ba439f43870b3d85f58dbfaf53
Diogo Souza <diogo.souza@uds.com.br>
2020-11-28 23:12:41 -0400

    chore: implementado método de criação no repositorio de contato

--- src/domain/repositories/contact.repository.ts
+++ src/domain/repositories/contact.repository.ts
@@ -1,5 +1,17 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { Contact } from 'Domain/entities';
+import { ContactDto } from 'Domain/dtos/contact.dto';
 
 @EntityRepository(Contact)
-export class ContactRepository extends Repository<Contact> {}
+export class ContactRepository extends Repository<Contact> {
+  async createContact(contactDto: ContactDto): Promise<Contact> {
+    try {
+      const contact = this.create(contactDto);
+
+      return await this.save(contact);
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+}
